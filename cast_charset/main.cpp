bd61e3dbe9cb77d178940429c4eff7cb598f7c28
nikolaAV <alexander.nikolaenko@gmail.com>
2017-02-05 14:34:45 +0200

    cast_charset::test update

--- cast_charset/main.cpp
+++ cast_charset/main.cpp
@@ -13,8 +13,33 @@ int main()
         wcout   << ws << endl;
     }
 
+    try
     { // test 2
-            
-    }
+#ifdef _MSC_VER
+      // string with German umlaut and euro symbol (in Windows encoding):
+      // http://www.science.co.il/Language/Character-code.asp?s=1252
 
+       const auto& loc = locale("deu_DEU.1252");
+       const string original = "nj: \xE4 + \x80 1";
+#else
+      // string with German umlaut and euro symbol (in ISO Latin-15 encoding):
+      // http://www.science.co.il/Language/Character-sets.asp
+
+       const auto& loc = locale("deu_DEU.ISO-8859-15");
+       const string original = "nj: \xE4 + \xA4 1";
+#endif
+
+       cout << endl << "default locale: " << original;
+       cout.imbue(loc);
+       cout << endl << "german locale: " << original;
+
+       const auto w = to_wstring(original, loc);
+       wcout << endl <<  w << endl;
+
+       assert(original==to_string(w,loc));
+    }
+    catch(const std::exception& e)
+    {
+       cout << e.what() << endl;
+    }
 }
