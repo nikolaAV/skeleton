9260730be5b27e8adde10179ae56c40c31cc1c38
nikolaAV <alexander.nikolaenko@gmail.com>
2018-09-04 13:31:40 +0300

    update

--- user_extensible_error/main.cpp
+++ user_extensible_error/main.cpp
@@ -1,10 +1,39 @@
 #include <system_error>
-#include <future>
-#include <iostream>
 
 /**
    http://blog.think-async.com/2010/04/system-error-support-in-c0x-part-4.html
 
+   Steps that are required to provide your own errors
+   1) define set of error constants of enum type that are specific you domain scope
+         enum class myerrc
+         {err1,err2,err3,...};
+   2) define an error_category class which indicates a source of errors. 
+      The category and a myerrc constant is the pair that uniquely represets your error
+         class my_error_category : std::error_category
+         {...}
+   3) give the category a human-readable name
+      const char* my_error_category::name() const override 
+         {  return "my_category"; }
+   4) every myerrc constant should have string representation
+         std::string my_error_category::message(int code) const override {
+            switch(code) 
+            {
+               case myerrc::err1: return "err1";
+               ...
+   5) uniquely identify your category. The identity of an error_category-derived object (my_error_category) is determined by its address
+         static const std::error_category& my_error_category::::instance() {
+            static my_error_category instance{};
+            return instance;
+         }
+   6) construct an std::error_code from the myerrc
+         inline std::error_code make_error_code(myerrc e) {
+            return std::error_code(e,my_error_category::instance());
+         }
+   7) register for implicit conversion to std::error_code. We have to specify 'enum class myerrc' contains error constants of our domain scope
+         namespace std
+         {
+            template <> struct is_error_code_enum<myerrc> : public std::true_type {};
+         }
 */
 
 namespace http
@@ -15,12 +44,12 @@ namespace http
 
    enum class errc
    {
-     continue_request = 100,
-     switching_protocols = 101,
-     ok = 200,
+     continue_request      = 100,
+     switching_protocols   = 101,
+     ok                    = 200,
      // ...
-     forbidden = 403,
-     gateway_timeout = 504,
+     forbidden             = 403,
+     gateway_timeout       = 504,
      version_not_supported = 505
    };
 
@@ -29,56 +58,42 @@ namespace http
       error_category() = default;
 
    public:
-      static const std::error_category& instance() noexcept;
+      static const std::error_category& instance() noexcept {
+         static error_category instance{};
+         return instance;
+      }
 
-      const char*          name() const  noexcept override;
-      std::string          message(int) const override;
-      std::error_condition default_error_condition(int) const noexcept override;
+      const char*          name() const  noexcept override {
+         return "http"; 
+      }
+      std::string          message(int code) const override {
+         switch(static_cast<errc>(code)) 
+         {
+            case errc::continue_request:
+               return "Continue";
+            case errc::switching_protocols:
+               return "Switching protocols";
+            case errc::ok:
+               return "OK";
+            //...
+            case errc::forbidden:
+               return "Forbidden";
+            case errc::gateway_timeout:
+               return "Gateway time-out";
+            case errc::version_not_supported:
+               return "HTTP version not supported";
+         }
+      }
+      std::error_condition default_error_condition(int code) const noexcept override {
+         switch (static_cast<errc>(code))
+         {
+            case errc::forbidden:
+               return std::errc::permission_denied;
+            default:
+               return std::error_condition(code, *this);
+         }
+      }
    };
-
-   inline const std::error_category& error_category::instance() noexcept 
-   {
-      static error_category instance{};
-      return instance;
-   }
-
-   inline const char* error_category::name() const noexcept 
-   { 
-      return "http"; 
-   }
-
-   inline std::string error_category::message(int ev) const 
-   {
-      switch(static_cast<errc>(ev)) 
-      {
-         case errc::continue_request:
-            return "Continue";
-         case errc::switching_protocols:
-            return "Switching protocols";
-         case errc::ok:
-            return "OK";
-         //...
-         case errc::forbidden:
-            return "Forbidden";
-         case errc::gateway_timeout:
-            return "Gateway time-out";
-         case errc::version_not_supported:
-            return "HTTP version not supported";
-         default:
-            return "Unknown HTTP error";
-      }
-   }   
-
-   inline std::error_condition error_category::default_error_condition( int ev ) const noexcept 
-   {
-      switch (static_cast<errc>(ev))
-      {
-         case errc::forbidden:
-            return std::errc::permission_denied;
-         default:
-            return std::error_condition(ev, *this);
-      }
-   }
 
    inline std::error_code make_error_code(errc e) noexcept
    {
@@ -87,22 +102,18 @@ namespace http
          error_category::instance());
    }
 
-   inline std::error_condition make_error_condition(errc e) noexcept
-   {
-      return std::error_condition(
-         static_cast<int>(e),
-         error_category::instance());
-   }
-
-
 }  // namespace http  
 
 namespace std
 {
-  template <>
-  struct is_error_code_enum<http::errc>
-    : public true_type {};
-}
+   template <>
+   struct is_error_code_enum<http::errc>
+      : public true_type {};
+
+}  // namespace std 
+
+#include <future>
+#include <iostream>
 
 
 void process(const std::error_code& ec)
