b6fb84de026bcf67da5f06799d89a03882bb3688
nikolaAV <alexander.nikolaenko@gmail.com>
2019-01-24 13:48:58 +0200

    [trim] discarded symbols returned

--- algorithm/trim/main.cpp
+++ algorithm/trim/main.cpp
@@ -2,52 +2,55 @@
 #include <iterator>
 
 template<typename Range, typename UnaryPredicate>
-void trim_left_if(Range& range, UnaryPredicate pred)
+auto trim_left_if(Range& range, UnaryPredicate pred)
 {
    const auto first  = std::begin(range);
    const auto last   = std::end(range);
    const auto found  = std::find_if_not(first,last,pred);
-   if(first!=found)
+   const auto dist   = std::distance(first,found);
+   if(dist)
       range.erase(first,found);
+   return dist;
 }
 
 template<typename Range, typename T>
-void trim_left(Range& range, const T& value)
+auto trim_left(Range& range, const T& value)
 {
-   trim_left_if(range,[&value](const auto& v) {
+   return trim_left_if(range,[&value](const auto& v) {
       return value==v;
    });
 }
 
 template<typename Range, typename UnaryPredicate>
-void trim_right_if(Range& range, UnaryPredicate pred)
+auto trim_right_if(Range& range, UnaryPredicate pred)
 {
    const auto first  = std::rbegin(range);
    const auto last   = std::rend(range);
    const auto found  = std::find_if_not(first,last,pred);
+   const auto dist   = std::distance(found.base(),std::end(range));
    if(first!=found)
       range.erase(found.base(),std::end(range));
+   return dist;
 }
 
 template<typename Range, typename T>
-void trim_right(Range& range, const T& value)
+auto trim_right(Range& range, const T& value)
 {
-   trim_right_if(range,[&value](const auto& v) {
+   return trim_right_if(range,[&value](const auto& v) {
       return value==v;
    });
 }
 
 template<typename Range, typename UnaryPredicate>
-void trim_if(Range& range, UnaryPredicate pred)
+auto trim_if(Range& range, UnaryPredicate pred)
 {
-   trim_left_if (range,pred);
-   trim_right_if(range,pred);
+   return trim_left_if(range,pred) + trim_right_if(range,pred);
 }
 
 template<typename Range, typename T>
-void trim(Range& range, const T& value)
+auto trim(Range& range, const T& value)
 {
-   trim_if(range,[&value](const auto& v) {
+   return trim_if(range,[&value](const auto& v) {
       return value==v;
    });
 }
@@ -63,14 +66,15 @@ using namespace std;
 int main()
 {
    wstring blank_padded_edit_box = L"  Hello, World!   ";
-   trim(blank_padded_edit_box,' ');
+   const auto discarded = trim(blank_padded_edit_box,' ');
    assert(blank_padded_edit_box==L"Hello, World!");
+   assert(discarded==5);
 
    string pin = "abce{2301X*&%-";   // https://en.wikipedia.org/wiki/Personal_identification_number
-   trim_if(pin,[](const auto& ch) {
+   const auto n = trim_if(pin,[](const auto& ch) {
       return !isdigit(ch);          // <-- decimal digits are only expected
    });
-   assert(pin=="2301");
+   assert(pin=="2301" && n==10);
 
    list<int> bounded_sequence = { 1,3,6,-9,6,-23, 0,1,2,3,4,5,6,7,8,9,0, -100,99,7,4,2,56 }; 
    trim_if(bounded_sequence,[](int v){
