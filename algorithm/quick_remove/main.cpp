b2b928595715511098fbb1d4e24199274daee5cf
nikolaAV <alexander.nikolaenko@gmail.com>
2019-01-31 13:33:08 +0200

    unstable_remove

--- algorithm/quick_remove/main.cpp
+++ algorithm/quick_remove/main.cpp
@@ -1,94 +1,93 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <iterator>
 #include <cassert>
+#include <utility>
 
-
-namespace quick
-{
+/**
+   Removes all elements satisfying specific criteria from the std::vector.   
+   Removing is done without shifting the elements in the vector.
+   After removing the relative order of the elements is not preserved.
+*/
 
 template<typename T>
-void remove_at(std::vector<T>& vec, std::size_t idx)
-{
+void unstable_remove_at(std::vector<T>& vec, std::size_t idx) {
    assert(idx<vec.size());
-
    vec[idx]=std::move(vec.back());
    vec.pop_back();
 }
 
 template<typename T>
-void remove_at(std::vector<T>& vec, typename std::vector<T>::iterator it)
-{
+void unstable_remove_at(std::vector<T>& vec, typename std::vector<T>::iterator it) {
    assert(0<=std::distance(vec.begin(),it));
    assert(0< std::distance(it,vec.end()));
-
    *it=std::move(vec.back());
    vec.pop_back();
 }
 
-namespace details_
+template<typename T, typename UnaryPredicate>
+auto unstable_remove_if(std::vector<T>& vec, UnaryPredicate p)
 {
-   template<typename T, typename UnaryPredicate>
-   void trim_back(std::vector<T>& vec, UnaryPredicate p)
-   {
-      auto last = std::find_if_not(std::rbegin(vec),std::rend(vec),p);
-      vec.erase(last.base(),vec.end());
-   }
+   auto first = vec.begin();
+   auto last  = vec.end();
 
-   template<typename T, typename UnaryPredicate>
-   void remove_if(std::vector<T>& vec, UnaryPredicate p, size_t idx)
-   {
-      trim_back(vec,p);
-      for(size_t i=idx; i < vec.size(); ++i)
-         if(p(vec[i])) {
-            quick::remove_at(vec,i);
-            details_::remove_if(vec,p,++i);
-            return;
-         }
+   for (;;++first) {
+	   // Find the first instance of "p"...
+	   for (;;++first)
+		   if (first == last)
+			   return first;
+		   else if (p(*first))
+			   break;
+	   // ...and the last instance of "not p"...
+	   for (--last;;--last)
+		   if (first == last)
+			   return first;
+		   else if (!p(*last))
+			   break;
+	   // ...and move the latter over top of the former.
+	   *first = std::move(*last);
    }
-
-}  // details
+}
 
 template<typename T, typename UnaryPredicate>
-void remove_if(std::vector<T>& vec, UnaryPredicate p)
-{
-   details_::remove_if(vec,p,0);
+auto unstable_erase_if(std::vector<T>& vec, UnaryPredicate p) {
+   return vec.erase(unstable_remove_if(vec,p),vec.end());
 }
 
 template<typename T>
-void remove(std::vector<T>& vec, const T& value)
-{
-   quick::remove_if(vec,[&value](auto const& v){return v==value;});
+auto unstable_remove(std::vector<T>& vec, const T& value) {
+   return unstable_remove_if(vec,[&value](auto const& v){return v==value;});
 }
 
+template<typename T>
+auto unstable_erase(std::vector<T>& vec, const T& value) {
+   return unstable_erase_if(vec,[&value](auto const& v){return v==value;});
+}
 
-}  // end of namespace quick
+///////// Example of Usage
 
 using std::vector;
-using std::cin;
-using namespace quick;
 
 void test1()
 {
    vector<int> v {};
-   remove(v,3);
-   assert(0==v.size());
+   unstable_remove(v,3);
+   assert(v.empty());
    assert((vector<int>{})==v);
 }
 
 void test2()
 {
    vector<int> v {3,3,3,3,3,3,3,3,3,3};
-   remove(v,3);
-   assert(0==v.size());
+   unstable_erase(v,3);
+   assert(v.empty());
    assert((vector<int>{})==v);
 }
 
 void test3()
 {
    vector<int> v {1,2,3,3,3,3,3,3};
-   remove(v,3);
+   unstable_erase(v,3);
    assert(2==v.size());
    assert((vector<int>{1,2})==v);
 }
@@ -96,7 +95,7 @@ void test3()
 void test4()
 {
    vector<int> v {1,2,3};
-   remove(v,3);
+   unstable_erase(v,3);
    assert(2==v.size());
    assert((vector<int>{1,2})==v);
 }
@@ -104,7 +103,7 @@ void test4()
 void test5()
 {
    vector<int> v {1,2,4,5,6};
-   remove(v,3);
+   unstable_erase(v,3);
    assert(5==v.size());
    assert((vector<int>{1,2,4,5,6})==v);
 }
@@ -112,7 +111,7 @@ void test5()
 void test6()
 {
    vector<int> v {1,2,3};
-   remove(v,1);
+   unstable_erase(v,1);
    assert(2==v.size());
    assert((vector<int>{3,2})==v);
 }
@@ -120,7 +119,7 @@ void test6()
 void test11()
 {
    vector<int> v {1,2,3,4,5};
-   remove(v,3);
+   unstable_erase(v,3);
    assert(4==v.size());
    assert((vector<int>{1,2,5,4})==v);
 }
@@ -128,14 +127,14 @@ void test11()
 void test12()
 {
    vector<int> v {1,1,1,1,1};
-   remove(v,1);
-   assert(0==v.size());
+   unstable_erase(v,1);
+   assert(v.empty());
 }
 
 void test13()
 {
    vector<int> v {1,8,2,8,3,8,4,8,5,8};
-   remove(v,8);
+   unstable_erase(v,8);
    assert(5==v.size());
    assert((vector<int>{1,5,2,4,3})==v);
 }
@@ -143,7 +142,7 @@ void test13()
 void test14()
 {
    vector<int> v {1,8,2,8,3,8,4,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,5,8};
-   remove(v,8);
+   unstable_erase(v,8);
    assert(5==v.size());
    assert((vector<int>{1,5,2,4,3})==v);
 }
@@ -151,7 +150,7 @@ void test14()
 void test15()
 {
    vector<int> v {1,2,3,4,5,6,7,8,9,0};
-   remove_if(v, [](int val) {return val%2;} );
+   unstable_erase_if(v, [](int val) {return val%2;} );
    assert(5==v.size());
    assert((vector<int>{0,2,8,4,6})==v);
 }
