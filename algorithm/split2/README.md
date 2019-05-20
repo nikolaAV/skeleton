1b9dc1a7cf060467750d0f7ae63b6429bb4f7488
nikolaAV <alexander.nikolaenko@gmail.com>
2019-05-20 15:40:43 +0300

    Split2, "string_view" slider

--- algorithm/split2/README.md
+++ algorithm/split2/README.md
@@ -1,11 +1,27 @@
-# Split2 Under construction
-TBD. 
+# Split2, "string_view" slider
+This function moves a [`std::string_view`](https://en.cppreference.com/w/cpp/string/basic_string_view) slider between delimiter markers __without__ any unnecessary copies or data modifications. 
+The interface is very similar to [`std::for_each`](https://en.cppreference.com/w/cpp/algorithm/for_each) where an user-defined function object is to apply to every sequence of characters which are enclosed within specified delimeters - another sequence of characters.
 ```cpp
+template <typename StringView, typename UnaryOperation>
+UnaryOperation split(StringView s, StringView delim, UnaryOperation op)
 ```
 where 
+* s - the range `std::string_view` sequential characters to apply the function to
+* delim - the range `std::string_view` sequential characters specifying boundary marker
+* op - 	function object, to be applied to each occurrence separated by the boundary marker
 
 Usage example for split:
 ```cpp
+   struct callback {
+      vector<int> out;
+      void operator()(string_view s) {
+         int value{0};
+         from_chars(s.data(),s.data()+s.size(),value);
+         out.push_back(value);
+      }
+   };
+   const auto result = split("1<$>2<$>3<$>4<$>5","<$>",callback{});
+   assert((vector{1,2,3,4,5})==result.out);
 ```
 
 ## Further informations
--- algorithm/split2/main.cpp
+++ algorithm/split2/main.cpp
@@ -1,42 +1,80 @@
 #include <string_view>
+#include <utility>
 
 /**
 	TBD
 
-	\see 
+	\see https://github.com/nikolaAV/skeleton/tree/master/algorithm/split2
 */
 
-template <typename UnaryOperation>
-void split(std::string_view s, std::string_view delim, UnaryOperation op)
+namespace details
 {
-   using sv = std::string_view;
-   auto op_not_empty = [&op](auto s) { if(s.size()) op(s); };
+
+template <typename UnaryOperation, typename CharT, typename Traits = std::char_traits<CharT> >
+UnaryOperation split(std::basic_string_view<CharT,Traits> s, std::basic_string_view<CharT,Traits> delim, UnaryOperation op)
+      noexcept( noexcept(op(std::basic_string_view<CharT,Traits>{})) )
+{
+   using sv = std::basic_string_view<CharT,Traits>;
+   const auto op_not_empty = [&op](auto s) { if(s.size()) op(s); };
    size_t pos = 0;
    while ((pos = s.find(delim)) != sv::npos) {
       op_not_empty(sv{s.data(),pos});
       s.remove_prefix(pos+delim.size());
    }
    op_not_empty(s);
+   return op;
+}
+
+template <typename UnaryOperation, typename StringView>
+UnaryOperation split(StringView s, StringView delim, UnaryOperation op)
+{
+   using CharT  = typename StringView::value_type;
+   using Traits = typename StringView::traits_type;
+   return split<UnaryOperation,CharT,Traits>(s,delim,op);
+}
+
+}  // end of namespace details
+
+template <typename UnaryOperation>
+UnaryOperation split(std::string_view s, std::string_view delim, UnaryOperation op)
+{
+   return details::split<UnaryOperation,decltype(s)>(s,delim,op);
+}
+
+template <typename UnaryOperation>
+UnaryOperation split(std::wstring_view s, std::wstring_view delim, UnaryOperation op)
+{
+   return details::split<UnaryOperation,decltype(s)>(s,delim,op);
+}
+
+template <typename UnaryOperation>
+UnaryOperation split(std::u16string_view s, std::u16string_view delim, UnaryOperation op)
+{
+   return details::split<UnaryOperation,decltype(s)>(s,delim,op);
+}
+
+template <typename UnaryOperation>
+UnaryOperation split(std::u32string_view s, std::u32string_view delim, UnaryOperation op)
+{
+   return details::split<UnaryOperation,decltype(s)>(s,delim,op);
 }
 
 // Usage
 
-#include "stdwarnings_suppress_on.h"
 #include <string>
 #include <cassert>
 #include <vector>
 #include <charconv>
 #include <optional>
-#include "stdwarnings_suppress_off.h"
 
 using namespace std;
 
 void test1()
 {
-   vector<string> out;
+   vector<string>       out;
    const vector<string> expected {"scott","tiger","mushroom"} ;
-   split("scott>=tiger>=mushroom>=",">=",[&out](string_view s){
-      out.push_back(string{s});
+   split("scott--->tiger--->mushroom--->","--->",[&out](string_view s){
+      out.emplace_back(s.data(),s.size());
    });
    assert(expected==out);
 }
@@ -52,19 +90,22 @@ void test2()
 
 void test3()
 {
-   vector<int> out;
-   split("1<delimeter>2<delimeter>3<delimeter>4<delimeter>5","<delimeter>",[&out](string_view s){
-      int value;
-      from_chars(s.data(),s.data()+s.size(),value);
-      out.push_back(value);
-   });
-   assert((vector{1,2,3,4,5})==out);
+   struct callback {
+      vector<int> out;
+      void operator()(string_view s) {
+         int value{0};
+         from_chars(s.data(),s.data()+s.size(),value);
+         out.push_back(value);
+      }
+   };
+   const auto result = split("1<$>2<$>3<$>4<$>5","<$>",callback{});
+   assert((vector{1,2,3,4,5})==result.out);
 }
 
 void test4()
 {
-   optional<string> out;
-   split("!@#!@#!@#!@#!@#","!@#",[&out](string_view s){
+   optional<wstring> out;
+   split(L"!@#!@#!@#!@#!@#",L"!@#",[&out](auto const s){
       out = s;
    });
    assert(!out.has_value());
